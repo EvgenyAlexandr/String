9fa11127175142c2b6b44da9caa4dc5cf3e21459
EvgenyAlexand <bragin-e@mail.ru>
2025-07-26 01:21:36 +0700

    Д/З - Выполнение - Задача 1 решена.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        // Задача 1 - Формирование Ф.И.О.
+        String firstName  = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName   = "Ivanov";
+        String fullName   = lastName + " " + firstName + " " + middleName;
+        System.out.println("1. Ф. И. О. сотрудника — " + fullName);
+
 
 
 
