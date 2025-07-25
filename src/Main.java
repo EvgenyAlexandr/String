ed644d973b2a7260cc3bb5b526ee481572fb8ac4
EvgenyAlexand <bragin-e@mail.ru>
2025-07-26 01:26:38 +0700

    Д/З - Выполнение - Задача 2 решена.

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,18 @@ public class Main {
     public static void main(String[] args) {
 
         // Задача 1 - Формирование Ф.И.О.
+        System.out.println("Задача 1");
         String firstName  = "Ivan";
         String middleName = "Ivanovich";
         String lastName   = "Ivanov";
         String fullName   = lastName + " " + firstName + " " + middleName;
-        System.out.println("1. Ф. И. О. сотрудника — " + fullName);
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
 
+
+        // Задача 2 - Преобразование в верхний регистр
+        System.out.println("\nЗадача 2");
+        String reportFullName = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + reportFullName);
 
 
 
